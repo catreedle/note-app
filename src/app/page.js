5f5fb41727806a80de68a7e7e1b8ad758a566b8e
Purnama S Rahayu <purnamasrahayu@gmail.com>
2023-11-21 17:30:51 +0700

    add function delete and mark todo list

--- src/app/page.js
+++ src/app/page.js
@@ -7,12 +7,31 @@ import { useState, useEffect } from "react";
 export default function Home() {
   const [todo, setTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
+  const [isDoneList, setIsDoneList] = useState([]);
 
   function handleAddTodoList() {
     if (todo) {
       setTodoList([...todoList, todo])
+      setIsDoneList([...isDoneList, false]); // Initialize isDone as false for the new item
       setTodo('');
     }
+
+  }
+
+  function markAsDone(index) {
+    const updatedIsDoneList = [...isDoneList];
+    updatedIsDoneList[index] = !updatedIsDoneList[index];
+    setIsDoneList(updatedIsDoneList);
+  }
+
+  function deleteFromTodoList(index) {
+    const updatedTodoList = [...todoList];
+    const updatedIsDoneList = [...isDoneList];
+    updatedTodoList.splice(index, 1);
+    updatedIsDoneList.splice(index, 1);
+
+    setTodoList(updatedTodoList);
+    setIsDoneList(updatedIsDoneList);
 
   }
 
@@ -32,12 +51,12 @@ export default function Home() {
         <ul className=" w-full flex flex-col items-center">
           {todoList.map((item, index) => (
             <li className=" w-1/2 m-2 bg-sky-300 p-3 rounded-full flex justify-between" key={index}>
-              <span>
+              <span className={` ${isDoneList[index] ? 'line-through' : ''}`}>
                 {item}
               </span>
               <div className=" flex justify-between w-1/12">
-                <button className=" text-red-500 hover:text-red-700 focus:outline-none">X</button>
-                <button className=" text-sky-800 hover:text-sky-950 focus:outline-none">✓</button>
+                <button className=" text-red-500 hover:text-red-700 focus:outline-none" onClick={() => deleteFromTodoList(index)}>X</button>
+                <button className=" text-sky-800 hover:text-sky-950 focus:outline-none" onClick={() => markAsDone(index)}>✓</button>
               </div>
             </li>
           ))}
